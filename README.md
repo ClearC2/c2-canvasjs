a067ef3068b2524553561bc2897260f66df759ea
Jacob Allen <jacobrallen1@gmail.com>
2019-06-04 15:25:55 -0500

    feat(app): initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# C2 CanvasJS
+
+**Description:**
+Clear C2's implementation of CanvasJS
---
+++ example/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = props => {
+  return (
+    <div className='page-container '>
+      Template Plugin
+    </div>
+  )
+}
+
+export default App
